f9d4c3ae5678cff9d73d7f264867861f17e12279
Nikita Shinov <shinov_96@mail.ru>
2022-06-30 14:16:25 +0300

    Initial Commit

---
+++ RxWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxWeather
+//
+//  Created by max on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
